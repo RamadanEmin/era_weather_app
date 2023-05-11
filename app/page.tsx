5efa01ffb52da066209c45b8da9672daafc4a3cf
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-05-11 23:29:39 +0300

    Added Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
+'use client';
+
+import { Card, Divider, Subtitle, Text } from '@tremor/react';
+
 export default function Home() {
     return (
-        <main >
-            
-        </main>
+        <div className="min-h-screen bg-gradient-to-br from-[#394F68] to-[#183B7E] p-10 flex flex-col justify-center items-center">
+            <Card className='max-w-4xl mx-auto'>
+                <Text className='text-6xl font-bold text-center mb-10'>Weather AI</Text>
+                <Subtitle className='text-xl text-center'>Powered by OpenAI, Next.js 13.3, Tailwind, Tremor 2.0 + more!</Subtitle>
+                <Divider className='my-10' />
+
+                <Card className='bg-gradient-to-br from-[#394F68] to-[#183B7E]'>
+                    <CityPicker />
+                </Card>
+            </Card>
+        </div>
     );
 }
--- next.config.js
+++ next.config.js
@@ -2,8 +2,11 @@
 const nextConfig = {
   experimental: {
     appDir: true,
+    serverComponentsExternalPackages: ['@tremor/react'],
   },
-
+  images: {
+    domains: ['www.weatherbit.io']
+  }
 };
 
 module.exports = nextConfig;
