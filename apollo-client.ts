bd3c4786c1fc873c4b562ae4abbc6a415266781f
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-05-13 11:09:37 +0300

    Implementing Apollo Client

---
+++ apollo-client.ts
@@ -0,0 +1,14 @@
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+
+let client: ApolloClient<any> | null = null;
+
+export const getClient = () => {
+  const client = new ApolloClient({
+    uri: process.env.API_URL,
+    cache: new InMemoryCache(),
+    headers: {
+      Authorization: `apikey ${process.env.NEXT_PUBLIC_STEPZEN_API_KEY}`
+    }
+  });
+  return client;
+};
