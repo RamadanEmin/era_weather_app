d32b4a2087f5858ca1b5b7b9602aaf381f393610
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-05-16 17:47:03 +0300

    Added 'Show temperature' functionality

--- components/InformationPanel.tsx
+++ components/InformationPanel.tsx
@@ -42,6 +42,26 @@ const InformationPanel = ({ city, lat, long, results }: Props) => {
                     })}
                 </p>
             </div>
+            <hr className='mt-10 mb-5' />
+
+            <div className='flex items-center justify-between'>
+                <div>
+                    <Image
+                        src={`https://www.weatherbit.io/static/img/icons/${weatherToCodeString[results.current_weather.weathercode].icon}.png`}
+                        alt={weatherToCodeString[results.current_weather.weathercode].label}
+                        width={75}
+                        height={75}
+                    />
+                    <div className='flex items-center justify-between space-x-10'>
+                        <p className='text-6xl font-semibold'>
+                            {results.current_weather.temperature.toFixed(1)}˚C
+                        </p>
+                        <p className='text-right font-extralight text-lg'>
+                            {weatherToCodeString[results.current_weather.weathercode].label}
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
