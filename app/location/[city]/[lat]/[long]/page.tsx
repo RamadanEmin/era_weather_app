6557eb93c0ef1b5eb6d1dee77825e13d68df1dfa
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-05-22 12:47:54 +0300

    Added fetching data from 'getBasePath'

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -1,103 +1,121 @@
-import { getClient } from "@/apollo-client";
-import CalloutCard from "@/components/CalloutCard";
+import { getClient } from '@/apollo-client';
+import CalloutCard from '@/components/CalloutCard';
 import HumidityChart from '@/components/HumidityChart';
-import InformationPanel from "@/components/InformationPanel";
+import InformationPanel from '@/components/InformationPanel';
 import RainChart from '@/components/RainChart';
-import StatCard from "@/components/StatCard";
+import StatCard from '@/components/StatCard';
 import TempChart from '@/components/TempChart';
-import fetchWeatherQuery from "@/graphql/queries/fetchWeatherQueries";
+import fetchWeatherQuery from '@/graphql/queries/fetchWeatherQueries';
+import { cleanData } from '@/lib/clearData';
+import { getBasePath } from '@/lib/getBasePath';
 
 export const revalidate = 60;
 
 type Props = {
-  params: {
-    city: string;
-    lat: string;
-    long: string;
-  };
-};
+    params: {
+        city: string;
+        lat: string;
+        long: string;
+    }
+}
 
 async function WeatherPage({ params: { city, lat, long } }: Props) {
-  const client = getClient();
+    const client = getClient();
 
-  const { data } = await client.query({
-    query: fetchWeatherQuery,
-    variables: {
-      current_weather: "true",
-      latitude: lat,
-      longitude: long,
-      timezone: "EET",
-    },
-  });
+    const { data } = await client.query({
+        query: fetchWeatherQuery,
+        variables: {
+            current_weather: 'true',
+            latitude: lat,
+            longitude: long,
+            timezone: 'EET'
+        }
+    });
 
-  const results: Root = data.myQuery;
+    const results: Root = data.myQuery;
 
-  console.log(results.daily);
+    console.log(results.daily.sunrise);
+    
 
-  return (
-    <div className="flex flex-col min-h-screen md:flex-row">
-      <InformationPanel city={city} lat={lat} long={long} results={results} />
-      <div className="flex-1 p-5 lg:p-10">
-        <div className="p-5">
-          <div className="pb-5">
-            <h2 className="text-xl font-bold">Todays Overview</h2>
-            <p className="text-sm text-gray-400">
-              Last Update at:{" "}
-              {new Date(results.current_weather.time).toLocaleString()} (
-              {results.timezone})
-            </p>
-          </div>
-          <div className="m-2 mb-10">
-            <CalloutCard message="This is where GPT-4 Summary will go!" />
-          </div>
-          <div className="grid grid-cols-1 xl:grid-cols-2 gap-5 m-2">
-            <StatCard
-              title="Maximum Temperature"
-              metric={`${results.daily.temperature_2m_max[0].toFixed(1)}˚`}
-              color="yellow"
-            />
+    const dataToSend = cleanData(results, city);
 
-            <StatCard
-              title="Minimum Temperature"
-              metric={`${results.daily.temperature_2m_min[0].toFixed(1)}˚`}
-              color="green"
-            />
-          </div>
-          <div>
-            <StatCard
-              title="UV Index"
-              metric={results.daily.uv_index_max[0].toFixed(1)}
-              color="rose"
-            />
-            {Number(results.daily.uv_index_max[0].toFixed(1)) > 5 && (
-              <CalloutCard
-                message={"The UV is high today, be sure to wear SPF!"}
-                warning
-              />
-            )}
-          </div>
-          <div className="flex space-x-3">
-            <StatCard
-              title="Wind Speed"
-              metric={`${results.current_weather.windspeed.toFixed(1)}m/s`}
-              color="cyan"
-            />
+    const res = await fetch(`${getBasePath()}/api/getWeatherSummary`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ weatherData: dataToSend })
+    });
 
-            <StatCard
-              title="Wind Direction"
-              metric={`${results.current_weather.winddirection.toFixed(1)}˚`}
-              color="violet"
-            />
-          </div>
+    const GPTdata = await res.json();
+    const { content } = GPTdata;
+
+    return (
+        <div className='flex flex-col min-h-screen md:flex-row'>
+            <InformationPanel city={city} lat={lat} long={long} results={results} />
+            <div className='flex-1 p-5 lg:p-10'>
+                <div className='p-5'>
+                    <div className='pb-5'>
+                        <h2 className='text-xl font-bold'>Todays Overview</h2>
+                        <p className='text-sm text-gray-400'>
+                            Last Update at: {' '}
+                            {new Date(results.current_weather.time).toLocaleString()} ({results.timezone})
+                        </p>
+                    </div>
+
+                    <div className='m-2 mb-10'>
+                        <CalloutCard message={content} />
+                    </div>
+                    <div className='grid grid-cols-1 xl:grid-cols-2 gap-5 m-2'>
+                        <StatCard
+                            title='Maximum Temperature'
+                            metric={`${results.daily.temperature_2m_max[0].toFixed(1)}˚`}
+                            color='yellow'
+                        />
+
+                        <StatCard
+                            title='Minimum Temperature'
+                            metric={`${results.daily.temperature_2m_min[0].toFixed(1)}˚`}
+                            color='green'
+                        />
+
+                        <div>
+                            <StatCard
+                                title='UV Index'
+                                metric={results.daily.uv_index_max[0].toFixed(1)}
+                                color='rose'
+                            />
+                            {Number(results.daily.uv_index_max[0].toFixed(1)) > 5 && (
+                                <CalloutCard
+                                    message={'The UV is high today, be sure to wear SPF!'}
+                                    warning
+                                />
+                            )}
+                        </div>
+
+                        <div className='flex space-x-3'>
+                            <StatCard
+                                title='Wind Speed'
+                                metric={`${results.current_weather.windspeed.toFixed(1)}m/s`}
+                                color='cyan'
+                            />
+
+                            <StatCard
+                                title='Wind Direction'
+                                metric={`${results.current_weather.winddirection.toFixed(1)}˚`}
+                                color='violet'
+                            />
+                        </div>
+                    </div>
+                </div>
+                <hr className='mb-5' />
+                <div className='space-y-3'>
+                    <TempChart results={results} />
+                    <RainChart results={results} />
+                    <HumidityChart results={results} />
+                </div>
+            </div>
         </div>
-      </div>
-      <hr className="mb-5" />
-      <div className="space-y-3">
-        <TempChart results={results} />
-        <RainChart results={results} />
-        <HumidityChart results={results} />
-      </div>
-    </div>
-  );
-}
+    );
+};
 export default WeatherPage;
